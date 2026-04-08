ae007838e39542d8b51db581f8014a850e387a7b
huang <ubitcode@163.com>
2026-04-08 18:16:06 +0800

    添加启动页遮蔽

--- app/src/main/java/com/ubitar/testcompose/MainActivity.kt
+++ app/src/main/java/com/ubitar/testcompose/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.ubitar.testcompose
 
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -12,7 +14,14 @@ import com.ubitar.testcompose.ui.theme.TestComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        val launchTime = SystemClock.elapsedRealtime()
         super.onCreate(savedInstanceState)
+
+        splashScreen.setKeepOnScreenCondition {
+            SystemClock.elapsedRealtime() - launchTime < 1_000L
+        }
+
         enableEdgeToEdge()
         setContent {
             TestComposeTheme {
